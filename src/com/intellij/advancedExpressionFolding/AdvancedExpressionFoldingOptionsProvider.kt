848e4eda824b276d1c038d464a7f0eb272f5874b
Antoni <antoni2work@gmail.com>
2024-03-02 19:00:38 +0100

    Options provider clean-up

--- src/com/intellij/advancedExpressionFolding/AdvancedExpressionFoldingOptionsProvider.kt
+++ src/com/intellij/advancedExpressionFolding/AdvancedExpressionFoldingOptionsProvider.kt
@@ -14,30 +14,35 @@ class AdvancedExpressionFoldingOptionsProvider protected constructor() :
         title = "Advanced Expression Folding 2"
         checkBox("Getters and setters as properties", myState::getSetExpressionsCollapse)
         checkBox("Variable declarations (var/val)", myState::varExpressionsCollapse)
+        checkBox("Compact control flow condition syntax (Golang ifs)", myState::compactControlFlowSyntaxCollapse)
+        checkBox("List.get, List.set, Map.get and Map.put expressions, array and list literals", myState::getExpressionsCollapse)
 
-        checkBox("Math, BigDecimal and BigInteger expressions (deprecated)", myState::arithmeticExpressionsCollapse)
         checkBox("StringBuilder.append and Collection.add/remove expressions, interpolated Strings and Stream expressions", myState::concatenationExpressionsCollapse)
         checkBox("List.subList and String.substring expressions", myState::slicingExpressionsCollapse)
         checkBox("Object.equals and Comparable.compareTo expressions", myState::comparingExpressionsCollapse)
+
         checkBox("Java.time isBefore/isAfter expressions", myState::comparingLocalDatesCollapse)
         checkBox("LocalDate.of literals (e.g. 2018-02-12)", myState::localDateLiteralCollapse)
         checkBox("Postfix LocalDate literals (e.g. 2018Y-02M-12D)", myState::localDateLiteralPostfixCollapse)
-        checkBox("List.get, List.set, Map.get and Map.put expressions, array and list literals", myState::getExpressionsCollapse)
+
+        checkBox("Type cast expressions", myState::castExpressionsCollapse)
         checkBox("For loops, range expressions", myState::rangeExpressionsCollapse)
         checkBox("Null-safe calls", myState::checkExpressionsCollapse)
-        checkBox("Type cast expressions", myState::castExpressionsCollapse)
-        checkBox("Control flow single-statement code block braces (read-only files)", myState::controlFlowSingleStatementCodeBlockCollapse)
-        checkBox("Control flow multi-statement code block braces (read-only files, deprecated)", myState::controlFlowMultiStatementCodeBlockCollapse)
-        checkBox("Compact control flow condition syntax", myState::compactControlFlowSyntaxCollapse)
-        checkBox("Semicolons (read-only files)", myState::semicolonsCollapse)
+        checkBox("Kotlin quick return", myState::kotlinQuickReturn)
+        checkBox("Extended null-safe ifs", myState::ifNullSafe)
+
         checkBox("Asserts", myState::assertsCollapse)
+
         checkBox("Display optional as Kotlin null-safe", myState::optional)
         checkBox("Display stream operations as Groovy's spread operator", myState::streamSpread)
+
         checkBox("Display Java bean as Lombok", myState::lombok)
         checkBox("Display mapping of field with same name as << (for builders, setters and assignments)", myState::fieldShift)
-        checkBox("Kotlin quick return", myState::kotlinQuickReturn)
-        checkBox("Extended null-safe ifs (experimental)", myState::ifNullSafe)
         checkBox("Log folding (experimental)", myState::logFolding)
+
+        checkBox("Control flow single-statement code block braces (read-only files)", myState::controlFlowSingleStatementCodeBlockCollapse)
+        checkBox("Control flow multi-statement code block braces (read-only files, deprecated)", myState::controlFlowMultiStatementCodeBlockCollapse)
+        checkBox("Semicolons (read-only files)", myState::semicolonsCollapse)
     }
 
 }
