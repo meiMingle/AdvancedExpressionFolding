f3eac045dc97d6692d80908f671745c4f176a37f
Antoni <antoni2work@gmail.com>
2024-03-05 18:55:51 +0100

    compatibility problem fixed

--- src/com/intellij/advancedExpressionFolding/AdvancedExpressionFoldingBuilder.java
+++ src/com/intellij/advancedExpressionFolding/AdvancedExpressionFoldingBuilder.java
@@ -14,6 +14,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AdvancedExpressionFoldingBuilder extends FoldingBuilderEx {
 
@@ -31,7 +32,7 @@ public class AdvancedExpressionFoldingBuilder extends FoldingBuilderEx {
                 + it.getPlaceholderText()
                 + "[" +
                 it.getGroup()
-                + "]").toList();
+                + "]").collect(Collectors.toList());
     }
 
     @Nullable
